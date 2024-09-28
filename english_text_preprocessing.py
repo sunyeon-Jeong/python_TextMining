b16555eeb53e1e53327a44a4eb0651d295ed6b7e
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-09-28 15:42:02 +0900

    Feat : week2(영어 텍스트 전처리) #3 - 영어 토큰화(단어/문장)

--- english_text_preprocessing.py
+++ english_text_preprocessing.py
@@ -19,7 +19,7 @@ nltk.download('averaged_perceptron_tagger')
 1. 토큰화
 '''
 '''
-1-1. 단어 토큰화
+1-1. 단어 토큰화 : nltk (word/WordPunct/TreebankWord_tokenize)
 - 텍스트 데이터의 특성과 사용 목적에 따라 토큰화 방법 선택
 - word_tokenize, WordPunctTokenizer, TreebankWordTokenizer 모듈
 '''
@@ -35,3 +35,21 @@ print('WordPunctTokenizer: ', WordPunctTokenizer().tokenize(text))
 
 # TreebankWordTokenizer 사용 (Penn Treebank Tokenization)
 print('TreebankWordTokenizer: ', TreebankWordTokenizer().tokenize(text))
+
+'''
+1-2. 문장 토큰화 : nltk (sent_tokenize)
+'''
+from nltk.tokenize import sent_tokenize
+
+text = """Cosmos is a Greek word for the order of the universe.
+It is, in a way, the opposite of Chaos.
+It implies the deep interconnectedness of all things.
+It conveys awe for the intricate and subtle way in which the universe is put together."""
+
+# 문장 단위로 텍스트 분할
+print(sent_tokenize(text))
+
+text = "I am actively looking for Ph.D. students. and you are a Ph.D student."
+
+# 문장 단위로 텍스트 분할
+print(sent_tokenize(text))
