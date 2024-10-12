2d502468b85811bae391cda643e136e4d6eec0e1
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-10-12 11:38:59 +0900

    Feat : week4(카운트 기반 문서표현) #6 - TF-IDF

--- week4/count_vector_scratch.py
+++ week4/count_vector_scratch.py
@@ -58,3 +58,81 @@ vocab, bow = build_bag_of_words(doc4)
 
 print('vocabulary : ', vocab)
 print('bag of words vector : ', bow)
+
+
+'''
+2. TF-IDF (단어빈도-역문서빈도)
+- 특정 문서 내에서 단어 빈도가 높을수록 : 중요도 증가
+- 전체 문서들에서는 그 단어를 포함한 문서가 적을수록(흔하지 않을수록) : 점수 증가
+-> 모든 문서에 나타나는 흔한 단어의 중요도를 낮게 두는 방식
+'''
+import pandas as pd
+from math import log
+
+doc1 = '먹고 싶은 사과'
+doc2 = '먹고 싶은 바나나'
+doc3 = '길고 노란 바나나 바나나'
+doc4 = '저는 과일이 좋아요'
+
+docs = [doc1, doc2, doc3, doc4]
+
+# 총 문서의 수
+D = len(docs)
+
+# 문서 d 내에서(특정문서) 단어 t의 빈도(TF) 계산
+def tf(t, d):
+    return d.count(t)
+
+# 단어 t의 역문서 빈도(IDF) 계산
+def idf(t):
+    df = 0
+    
+    for doc in docs:
+        # doc에 t가 있는지 확인
+        df += t in doc
+    return log(D/(df+1))
+    # return log((D+1)/(df+1)) + 1
+
+# TF-IDF 점수 계산
+def tfidf(t, d):
+    return tf(t, d) * idf(t)
+
+# 문서 내에 등장하는 단어의 집합
+vocas = list(set(w for doc in docs for w in doc.split()))
+vocas.sort()
+
+vocas
+
+result = []
+
+for doc in docs:
+    result.append([])
+    
+    for voca in vocas:
+        result[-1].append(tf(voca, doc))
+
+tf_score = pd.DataFrame(result, columns=vocas)
+
+tf_score
+
+result = []
+
+for voca in vocas:
+    result.append(idf(voca))
+    
+idf_score = pd.DataFrame(result, index=vocas, columns=["IDF"])
+
+idf_score
+
+result = []
+
+for doc in docs:
+    result.append([])
+    
+    for voca in vocas:
+        result[-1].append(tfidf(voca, doc))
+        
+        
+tfidf_score = pd.DataFrame(result, columns=vocas)
+
+tfidf_score
